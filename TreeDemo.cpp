b9d9950d091333df47ccb5f700654eb75420afcd
tzvimints <zvimints@gmail.com>
2019-03-16 13:12:12 +0200

    Changed from ariel::

--- TreeDemo.cpp
+++ TreeDemo.cpp
@@ -13,14 +13,14 @@ using std::cout, std::endl;
 int main() {
   try {
     // constructs an empty tree:
-    ariel::Tree emptytree;
+    Tree emptytree;
     cout << "emptytree: size=" << emptytree.size() << endl; // should print 0
     
     // constructs an ordered binary tree where:
       // 5 is in the root;
       // 3 is the root's left child;
       // 7 is the root's right child.
-    ariel::Tree threetree;
+    Tree threetree;
     threetree.insert(5);
     threetree.insert(7);
     threetree.insert(3);
